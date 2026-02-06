74a42542ce262bc96c8e6c4612dbfa9d97cfaf99
jirineu <moizecape@gmail.com>
2026-02-06 00:10:20 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,8 @@
-self.addEventListener('fetch', function(event) {});
+// Este arquivo permite que o site seja instalável
+self.addEventListener('install', (e) => {
+    console.log('Service Worker instalado');
+});
+
+self.addEventListener('fetch', (event) => {
+    // Necessário para cumprir o requisito de PWA
+});
